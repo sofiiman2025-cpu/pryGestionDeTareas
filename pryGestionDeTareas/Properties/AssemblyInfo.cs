51d89fd94f8f364aefb766dc124917426a37db82
sofiiman2025-cpu <sofiiman2025@gmail.com>
2026-04-20 19:43:16 -0300

    Agregar archivos de proyecto.

---
+++ pryGestionDeTareas/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("pryGestionDeTareas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pryGestionDeTareas")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("79b930f5-74c0-4783-aca1-e4e1d2f7afee")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ pryGestionDeTareas/frmTareas.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace pryGestionDeTareas
+{
+    partial class frmTareas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTareas));
+            this.grpCarga = new System.Windows.Forms.GroupBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.lblDescripción = new System.Windows.Forms.Label();
+            this.lblCategoría = new System.Windows.Forms.Label();
+            this.lblPrioridad = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.lbl = new System.Windows.Forms.Label();
+            this.grpCarga.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpCarga
+            // 
+            this.grpCarga.Controls.Add(this.lbl);
+            this.grpCarga.Controls.Add(this.lblFecha);
+            this.grpCarga.Controls.Add(this.lblPrioridad);
+            this.grpCarga.Controls.Add(this.lblCategoría);
+            this.grpCarga.Controls.Add(this.lblDescripción);
+            this.grpCarga.Controls.Add(this.txtDescripcion);
+            this.grpCarga.Controls.Add(this.txtTitulo);
+            this.grpCarga.Controls.Add(this.lblTitulo);
+            this.grpCarga.Location = new System.Drawing.Point(12, 34);
+            this.grpCarga.Name = "grpCarga";
+            this.grpCarga.Size = new System.Drawing.Size(479, 410);
+            this.grpCarga.TabIndex = 1;
+            this.grpCarga.TabStop = false;
+            this.grpCarga.Text = "CARGA DE TAREAS";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(21, 47);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(47, 20);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Título";
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(119, 41);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(218, 26);
+            this.txtTitulo.TabIndex = 1;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(119, 104);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(218, 46);
+            this.txtDescripcion.TabIndex = 2;
+            // 
+            // lblDescripción
+            // 
+            this.lblDescripción.AutoSize = true;
+            this.lblDescripción.Location = new System.Drawing.Point(21, 110);
+            this.lblDescripción.Name = "lblDescripción";
+            this.lblDescripción.Size = new System.Drawing.Size(92, 20);
+            this.lblDescripción.TabIndex = 3;
+            this.lblDescripción.Text = "Descripción";
+            // 
+            // lblCategoría
+            // 
+            this.lblCategoría.AutoSize = true;
+            this.lblCategoría.Location = new System.Drawing.Point(21, 179);
+            this.lblCategoría.Name = "lblCategoría";
+            this.lblCategoría.Size = new System.Drawing.Size(78, 20);
+            this.lblCategoría.TabIndex = 4;
+            this.lblCategoría.Text = "Categoría";
+            // 
+            // lblPrioridad
+            // 
+            this.lblPrioridad.AutoSize = true;
+            this.lblPrioridad.Location = new System.Drawing.Point(21, 238);
+            this.lblPrioridad.Name = "lblPrioridad";
+            this.lblPrioridad.Size = new System.Drawing.Size(71, 20);
+            this.lblPrioridad.TabIndex = 5;
+            this.lblPrioridad.Text = "Prioridad";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(21, 298);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(164, 20);
+            this.lblFecha.TabIndex = 6;
+            this.lblFecha.Text = "Fecha de vencimiento";
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(21, 354);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(80, 20);
+            this.lbl.TabIndex = 7;
+            this.lbl.Text = "Asignar a:";
+            // 
+            // frmTareas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 601);
+            this.Controls.Add(this.grpCarga);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmTareas";
+            this.Text = "Gestión de tareas";
+            this.grpCarga.ResumeLayout(false);
+            this.grpCarga.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpCarga;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label lblPrioridad;
+        private System.Windows.Forms.Label lblCategoría;
+        private System.Windows.Forms.Label lblDescripción;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
+
